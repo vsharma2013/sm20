e3ad15eb162f5aa4567df517dd56fbdb96dbe219
Vishal Sharma <vishal2033@gmail.com>
2015-11-28 20:20:11 +0530

    RE empty proj, cc & le rule

--- rules/app1/orderFlow.js
+++ rules/app1/orderFlow.js
@@ -4,20 +4,35 @@ var oe = require('./orderEntities');
 
 var flow = nools.compile("orderFlow.nools", {
     define: {
-        item : oe.item
+        item : oe.item,
+        accountSplit : oe.accountSplit
     }
 });
 
 var session = flow.getSession();
 
-oe.defaultOrder.Items.forEach(function(item){
-	session.assert(new oe.item(item));
-});
 
-session.match(function(err){
-    if(err){
-        console.error(err.stack);
-    }else{
-        console.log("done");
-    }
-})
+
+function runMatch(cbOnDone){
+	session.match(function(err){
+	    if(err){
+	        console.error(err.stack);
+	        cbOnDone(false);
+	    }else{
+	        cbOnDone(true);
+	    }
+	})
+}
+
+function runItemRules(){
+	oe.defaultOrder.Items.forEach(function(item){
+		session.assert(new oe.item(item));
+	});
+	runMatch(runAllComplete)
+}
+
+function runAllComplete(){
+	console.log('executed all rules !!!!');
+}
+
+runItemRules();
