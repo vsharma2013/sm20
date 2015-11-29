ad9c07fef901675b252a03b160ac98222822d0a6
Vishal Sharma <vishal2033@gmail.com>
2015-11-29 17:06:06 +0530

    RE implemented rule#5

--- rules/app1/orderFlow.js
+++ rules/app1/orderFlow.js
@@ -14,18 +14,6 @@ var flow = nools.compile("orderFlow.nools", {
 
 var session = flow.getSession();
 
-function runMatch(cbOnDone){
-
-	session.match(function(err){
-	    if(err){
-	        console.error(err.stack);
-	        cbOnDone(false);
-	    }else{
-	        cbOnDone(true);
-	    }
-	})
-}
-
 function runItemRules(){
 	oe.defaultOrder.Items.forEach(function(item){
 		session.assert(new oe.item(item));
@@ -35,12 +23,24 @@ function runItemRules(){
 
 function runOrderRules(){
 	session.dispose();
+	session = flow.getSession();
 	session.assert(new oe.order(oe.defaultOrder));
 	runMatch(runAllComplete);
 }
 
 function runAllComplete(){
 	console.log('executed all rules !!!!\n');
+}
+
+function runMatch(cbOnDone){
+	session.match(function(err){
+	    if(err){
+	        console.error(err.stack);
+	        cbOnDone(false);
+	    }else{
+	        cbOnDone(true);
+	    }
+	})
 }
 
 console.log('\n');
