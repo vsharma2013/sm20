364b7d261100387ec2e1e219ddbc2c7d58324f00
Vishal Sharma <vishal2033@gmail.com>
2015-11-30 19:29:24 +0530

    cs order props render

--- customprops/app1/web/v1/index.js
+++ customprops/app1/web/v1/index.js
@@ -1,5 +1,5 @@
 function App(){
-
+	this.order = null;
 }
 var app = new App();
 
@@ -9,7 +9,38 @@ $(document).ready(function(){
 
 
 App.prototype.run = function(){
-	$.getJSON('/api/order', function(res){
+	$.getJSON('/api/order', (function(res){
 		console.log(res);
-	});
+		this.order = res;
+		this.render();
+	}).bind(this));
 }
+
+App.prototype.render = function(){
+	this.renderLeftContainer();
+	this.renderRightContainer();
+}
+
+App.prototype.renderLeftContainer = function(){
+
+}
+
+App.prototype.renderRightContainer = function(){
+	this.renderOrderProps();
+	this.renderItemProps();
+}
+
+App.prototype.renderOrderProps = function(){
+	for(var key in this.order){
+		if(typeof(this.order[key]) !== 'object'){
+			var html = '<div class="col-md-6"><div class="pull-left">ID_KEY</div><div class="pull-right prim-right">ID_VAL</div></div>';
+			html = html.replace('ID_KEY', key).replace('ID_VAL', this.order[key]);
+			$(html).appendTo($('.primary-container'))
+		}
+	}
+}
+
+App.prototype.renderItemProps = function(){
+
+}
+
