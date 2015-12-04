c883f2977b76ec65d8015fc99406df0a723f26bc
Vishal Sharma <vishal2033@gmail.com>
2015-12-04 11:41:47 +0530

    cp seperated ui schema from cp storage

--- customprops/app1/server/ApiRouter.js
+++ customprops/app1/server/ApiRouter.js
@@ -8,12 +8,24 @@ function ApiController(){
 
 ApiController.prototype.handleGetOrderRequest = function(req, res){
 	var id = parseInt(req.query.o);
-	dbMgr.getOrderById(id, function(doc){
+	dbMgr.getOrderById(id, true, function(doc){
         res.json(doc);
-	})	
+	});	
 }
 
+ApiController.prototype.handleGetRawOrderRequest = function(req, res){
+	var id = parseInt(req.query.o);
+	dbMgr.getOrderById(id, false, function(doc){
+        res.json(doc);
+	});	
+}
+
+ApiController.prototype.handleCustomPropsUISchemaRequest = function(req, res){
+	res.json(dbMgr.customPropsUISchema);
+}
 
 var apiController = new ApiController();
 router.get('/order', apiController.handleGetOrderRequest.bind(apiController));
+router.get('/orderraw', apiController.handleGetRawOrderRequest.bind(apiController));
+router.get('/cpuischema', apiController.handleCustomPropsUISchemaRequest.bind(apiController));
 module.exports = router;
--- customprops/app1/server/DefaultOrders.js
+++ customprops/app1/server/DefaultOrders.js
@@ -272,7 +272,174 @@ customPropsOrder.Items[2].customProps = [{
 	allValues : []
 }];
 
+var customPropsUISchema = {
+  order_category : {
+    type : 'ddlist',
+    label : 'Category',
+    allValues : ['OC-1', 'OC-2', 'OC-3']
+  },
+  order_priority :{
+    type : 'bool',
+    label : 'Is Urgent',
+    allValues : [0,1]
+  },
+  order_comments : {
+    type : 'text',
+    label : 'Comments',   
+    allValues : []
+  },
+  item_recurrence : {
+    type : 'singleoptionlist',
+    label : 'Repeat',
+    allValues : ['daily', 'weekly', 'monthly']
+  },
+  item_notify_to : {
+    type : 'multivaluelist',
+    label : 'Notify',
+    allValues : ['HR', 'Legal', 'Accounts', 'Admin', 'Marketing']
+  },
+  item_expiry : {
+    type : 'date',
+    label : 'Expiry Date',
+    allValues : []
+  }
+}
+
+var customPropsOrder2 = {
+  "Id": 3,
+  "PartnerId": 1,
+  "CreatedOn": "2015-11-11",
+  "CreatedBy": 1,
+  "Currency": "USD",
+  "PaymentTerm": 1,
+  "BusinessUnit": 1,
+  "Number": "2015-0000001",
+  "Name": "Direct",
+  "Items": [{
+    "Id": 1,
+    "SortOrder": 10,
+    "Name": "iPhone6",
+    "UnitPrice": 610.00,
+    "Taxes": 52.18,
+    "Shipping": 5.34,
+    "OtherCharges": 0.00,
+    "Quantity": 3.00,
+    "UOM": "EA",
+    "Partner": "e-Zone",
+    "Manufacturer": "Apple",
+    "Contracted": false,
+    "ContractedPrice": null,
+    "SplitType": "#",
+    "AccountingSplits": [{
+      "Id": 1,
+      "SortOrder": 100,
+      "SplitValue": "1",
+      "Project": "",
+      "CostCenter": "CC1",
+      "LegalEntity": "A-001",
+      "Department": "A-0000001" 
+    },
+    {
+      "Id": 2,
+      "SortOrder": 101,
+      "SplitValue": "2",
+      "Project": "P-1",
+      "CostCenter": "CC1",
+      "LegalEntity": "A-001",
+      "Department": "A-0000001" 
+    }]
+  },
+  {
+    "Id": 2,
+    "SortOrder": 15,
+    "Name": "Nestle Milk",
+    "UnitPrice": 1.00,
+    "Taxes": 0.08,
+    "Shipping": 0.04,
+    "OtherCharges": 0.01,
+    "Quantity": 20.00,
+    "UOM": "GA",
+    "Partner": "CostCo",
+    "Manufacturer": "Nestle",
+    "Contracted": true,
+    "ContractedPrice": 0.98,
+    "SplitType": "%",
+    "AccountingSplits": [{
+      "Id": 3,
+      "SortOrder": 102,
+      "SplitValue": "20",
+      "Project": "P-2",
+      "CostCenter": "CC2",
+      "LegalEntity": "A-001",
+      "Department": "A-0000002" 
+    },
+    {
+      "Id": 4,
+      "SortOrder": 100,
+      "SplitValue": "40",
+      "Project": "P-1",
+      "CostCenter": "CC1",
+      "LegalEntity": "A-001",
+      "Department": "A-0000001" 
+    },
+    {
+      "Id": 5,
+      "SortOrder": 101,
+      "SplitValue": "40",
+      "Project": "P-10",
+      "CostCenter": "CC10",
+      "LegalEntity": "A-002",
+      "Department": "A-2000001" 
+    }]
+  },
+  {
+    "Id": 3,
+    "SortOrder": 18,
+    "Name": "LED Bulb Phillips",
+    "UnitPrice": 52.28,
+    "Taxes": 2.18,
+    "Shipping": 5.34,
+    "OtherCharges": 0.00,
+    "Quantity": 2.00,
+    "UOM": "DZ",
+    "Partner": "BestBuy",
+    "Manufacturer": "GE",
+    "Contracted": true,
+    "ContractedPrice": 52.28,
+    "SplitType": "#",
+    "AccountingSplits": [{
+      "Id": 6,
+      "SortOrder": 100,
+      "SplitValue": "2",
+      "Project": "P-1",
+      "CostCenter": "CC1",
+      "LegalEntity": "A-001",
+      "Department": "A-0000011" 
+    }]
+  }]
+}
+
+customPropsOrder2.customProps = {
+  order_category : 'OC-2',
+  order_priority : 1,
+  order_comments : 'Send to marketing'
+}
+
+customPropsOrder2.Items[0].customProps = {
+  item_recurrence : 'weekly'
+}
+
+customPropsOrder2.Items[1].customProps = {
+  item_notify_to : ['HR', 'Admin']
+}
+
+customPropsOrder2.Items[2].customProps = {
+  item_expiry : '2016/06/30'
+}
+
 module.exports = {
 	order : order,
-	customPropsOrder : customPropsOrder
+	customPropsOrder : customPropsOrder,
+  customPropsOrder2 : customPropsOrder2,
+  customPropsUISchema : customPropsUISchema
 }
