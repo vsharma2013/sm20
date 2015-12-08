9c8f5a51c87399fbbc9e284a7d8e41b0375d790d
Vishal Sharma <vishal2033@gmail.com>
2015-12-08 19:44:24 +0530

    demo added server dir to app3

---
+++ customprops/app3/server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = new express();
+var apiRouter = require('./server/ApiRouter');
+
+app.use(require('body-parser').json())
+app.use(express.static('web'));
+app.use('/api', apiRouter);
+
+app.use(function(err, req, res, next) {
+  logger.log(err.stack);
+  res.status(500).json({success:false, data:'internal server error'});
+});
+
+var port = 1111;
+app.listen(port);
+console.log('server running at port - ' + port);
