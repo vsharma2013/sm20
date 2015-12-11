7066e67a8496d632da9b480b989b0e7c2db48bfb
Vishal Sharma <vishal2033@gmail.com>
2015-12-11 12:37:47 +0530

    WIP validations

--- customprops/app3/server/Validations.js
+++ customprops/app3/server/Validations.js
@@ -34,7 +34,7 @@ function hasDate(d){
 	}
 }
 
-var validations = {
+var validations_requisition = {
 	RequisitionName : {
 		validate : function(req){
 			return req.hasOwnProperty('RequisitionName') && hasString(req.RequisitionName) && req.RequisitionName.length <=200;
@@ -71,7 +71,10 @@ var validations = {
 			return req.customProps.hasOwnProperty('Markasurgent') && hasInt(req.customProps.Markasurgent);
 		},
 		err : 'Mark as urgent is a mandatory boolean field.'
-	},
+	}
+};
+
+var validations_item = {
 	Linenumber: {
 		validate : function(item){
 			return item.hasOwnProperty('Linenumber') && hasInt(item.Linenumber);
@@ -128,10 +131,176 @@ var validations = {
 		},
 		err : 'Item request date is a mandatory date type field.'
 	},
-	abc : {
-		validate : function(){
-
+	Needbydate : {
+		validate : function(item){
+			return item.hasOwnProperty('Needbydate') && hasDate(item.Needbydate) && Date.parse(item.Needbydate) > Date.now();
 		},
-		err : ''
+		err : 'Item need by date is a mandatory date type field. It should be greater current date' 
+	}
+};
+
+var validations_partner = {
+	Partnername : {
+		validate : function(partner){
+			return partner.hasOwnProperty('Partnername') && hasString(partner.Partnername);
+		},
+		err : 'Partner name should be a non-empty string value.'
+	},
+	Partnercode : {
+		validate : function(partner){
+			return partner.hasOwnProperty('Partnercode') && hasString(partner.Partnercode);
+		},
+		err : 'Partner code should be a non-empty string value.'
+	}
+};
+
+var validations_shipping = {
+	Shiptoname : {
+		validate : function(shipping){
+			return shipping.hasOwnProperty('Shiptoname') && hasString(shipping.Shiptoname);
+		},
+		err : 'Ship to name should be a non-empty string value.'
+	},
+	Shiptoaddress : {
+		validate : function(shipping){
+			return shipping.hasOwnProperty('Shiptoaddress') && hasString(shipping.Shiptoaddress);
+		},
+		err : 'Ship to address should be a non-empty string value.'
+	},
+	Deliverto : {
+		validate : function(shipping){
+			if(!shipping.customProps) return true;
+			if(!shipping.customProps.Deliverto) return true;
+			return shipping.customProps.Deliverto.length <= 2000;
+		},
+		err : 'Shipping deliver to field can have max 2000 characters.'
+	}
+};
+
+var validations_others = {
+	Procurementoption : {
+		validate : function(others){
+			if(!others.customProps) return false;
+			return others.customProps.hasOwnProperty('Procurementoption') && hasInt(others.customProps.Procurementoption);
+		},
+		err : 'Others procurement option should be a non-empty integer value.'
+	},
+	Capitalized : {
+		validate : function(others){
+			if(!others.customProps) return false;
+			var values = ['yes', 'no'];
+			return others.customProps.hasOwnProperty('Capitalized') && hasString(others.customProps.Capitalized)
+			       && _.contains(values, other.customProps.Capitalized.toLowerCase());
+		},
+		err : 'Others capitalized field should only have only Yes/No values.'
+	},
+	Billable : {
+		validate : function(others){
+			if(!others.customProps) return false;
+			var values = ['yes', 'no'];
+			return others.customProps.hasOwnProperty('Billable') && hasString(others.customProps.Billable)
+			       && _.contains(values, other.customProps.Billable.toLowerCase());
+		},
+		err : 'Others Billable field should only have only Yes/No values.'
+	},
+	Inventorytype : {
+		validate : function(others){
+			if(!others.customProps) return false;
+			return others.customProps.hasOwnProperty('Inventorytype') && hasInt(others.customProps.Inventorytype);
+		},
+		err : 'Others inventory type option should be a non-empty integer value.'
+	}
+};
+
+var validations_accounting = {
+	Type : {
+		validate : function(accounting){
+			return accounting.hasOwnProperty('Type') && hasInt(accounting.Type);
+		},
+		err : 'Accounting type should be a non-empty integer value.'
+	},
+	Quantity : {
+		validate : function(accounting){
+			return accounting.hasOwnProperty('Quantity') && hasFloat(accounting.Quantity) && accounting.Quantity > 0;
+		},
+		err : 'Accounting quantity should be a positive floating point value.'
+	},
+	Amount : {
+		validate : function(){
+			if(!accounting.Amount) return true;
+			return hasFloat(accounting.Amount);
+		},
+		err : 'Accounting amount should be a floating point value'
+	}
+};
+
+var validations_contract = {
+	Contractnumber : {
+		validate : function(contract){
+			if(!contract.Contractnumber) return true;
+			return hasString(contract.Contractnumber) && contract.Contractnumber.length <= 400;
+		},
+		err : 'Contract number should be a string value with max 400 characters'.
+	},
+	Contractname : {
+		validate : function(contract){
+			if(!contract.Contractname) return true;
+			return hasString(contract.Contractname) && contract.Contractname.length <= 400;
+		},
+		err : 'Contract name should be a string value with max 400 characters'.
+	},
+	Contractvalue : {
+		validate : function(contract){
+			if(!contract.Contractvalue) return true;
+			return hasFloat(contract.Contractvalue) && contract.Contractvalue > 0;
+		},
+		err : 'Contract value should be a positive floating point value'
+	}
+};
+
+function validateRequisition(requisition){
+	var items = [];
+	var requisitions = [requisition];
+	var partners = [];
+	var shippings = [];
+	var others = [];
+	var accountings = [];
+	var contracts = [];
+	var validationErrors = [];
+
+	for(var i = 0; i < requisition.Items.length; i++){
+		var item = requisition.Items[i]; items.push(item);
+		if(item.partner) partners.push(partner);
+		if(item.shipping) shippings.push(shipping);
+		if(item.others) others.push(others);
+		if(item.accounting) accountings.push(accounting);
+		if(item.contract) contracts.push(contract);
+	}
+	var vals = [];
+	vals = runValidation(requisitions, validations_requisition); validationErrors = validationErrors.concat(vals);
+	vals = runValidation(partners, validations_partner); validationErrors = validationErrors.concat(vals);
+	vals = runValidation(shippings, validations_shipping); validationErrors = validationErrors.concat(vals);
+	vals = runValidation(others, validations_others); validationErrors = validationErrors.concat(vals);
+	vals = runValidation(accountings, validations_accounting); validationErrors = validationErrors.concat(vals);
+	vals = runValidation(contracts, validations_accounting); validationErrors = validationErrors.concat(vals);
+
+	return {
+		success : validationErrors.length > 0 ? false : true,
+		message : validationErrors.join('\n');
 	}
 }
+
+function runValidation(validationObjects, validations){
+	if(!Array.isArray(validationObjects)) return [];
+
+	var validationErrors = [];
+	validationObjects.forEach(function(vObj){
+		for(var v in validations){
+			if(!v.validate(vObj))
+				validationErrors.push(v.err);
+		}
+	});
+	return validationErrors;
+}
+
+module.exports = validateRequisition;
