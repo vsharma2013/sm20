495c912ed3b532f5b3b12a76422cbe80bc591c49
Vishal Sharma <vishal2033@gmail.com>
2015-12-11 11:13:53 +0530

    WIP validations

--- customprops/app3/server/Validations.js
+++ customprops/app3/server/Validations.js
@@ -14,6 +14,26 @@ function hasInt(i){
 	}
 }
 
+function hasFloat(f){
+	try{
+		parseFloat(f);
+		return true;
+	}
+	catch(){
+		return false;
+	}
+}
+
+function hasDate(d){
+	try{
+		Date.parse(d);
+		return true;
+	}
+	catch(){
+		return false;
+	}
+}
+
 var validations = {
 	RequisitionName : {
 		validate : function(req){
@@ -52,5 +72,66 @@ var validations = {
 		},
 		err : 'Mark as urgent is a mandatory boolean field.'
 	},
+	Linenumber: {
+		validate : function(item){
+			return item.hasOwnProperty('Linenumber') && hasInt(item.Linenumber);
+		},
+		err : 'Line number should be an integer value.'
+	},
+	Itemnumber : {
+		validate : function(item){
+			if(!item.Itemnumber) return true;
+			return item.Itemnumber.length <= 100;
+		},
+		err : 'Item number can have max 100 characters.'
+	},
+	Item : {
+		validate : function(item){
+			return item.hasOwnProperty('Item') && hasString(item.Item) && item.Item.length <= 500;
+		},
+		err : 'Item should be a non-empty string value with max 500 characters.'
+	},
+	Partneritemnumber : {
+		validate : function(item){
+			if(!item.Partneritemnumber) return true;
+			return item.Partneritemnumber.length <= 100;
+		},
+		err : 'Partner item number can have max 100 characters.'
+	},
+	Quantity : {
+		validate : function(item){
+			return item.hasOwnProperty('Quantity') && hasFloat(item.Quantity) && item.Quantity > 0;
+		},
+		err : 'Quantity should be a non-negative floating point value.'
+	},
+	UOM : {
+		validate : function(item){
+			return item.hasOwnProperty('UOM') && hasString(item.UOM);
+		},
+		err : 'Item UOM should be a non-empty string value.'
+	},
+	Unitprice : {
+		validate : function(item){
+			return item.hasOwnProperty('Unitprice') && hasFloat(item.Unitprice) && item.Unitprice > 0;
+		},
+		err : 'Item unit price should be a non-zero poistive floating point value.'
+	},
+	Total : {
+		validate : function(item){
+			return item.hasOwnProperty('Total') && hasFloat(item.Total) && item.Total > 0;
+		},
+		err : 'Item total should be a non-zero poistive floating point value.'
+	},
+	Requesteddate : {
+		validate : function(item){
+			return item.hasOwnProperty('Requesteddate') && hasDate(item.Requesteddate);
+		},
+		err : 'Item request date is a mandatory date type field.'
+	},
+	abc : {
+		validate : function(){
 
+		},
+		err : ''
+	}
 }
