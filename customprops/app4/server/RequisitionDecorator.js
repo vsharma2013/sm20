fdf97fb714d6afdc7b9bc0917cad4f562c81cab0
Vishal Sharma <vishal2033@gmail.com>
2015-12-17 08:51:01 +0530

    Updating req decorator with latest code in app4

--- customprops/app4/server/RequisitionDecorator.js
+++ customprops/app4/server/RequisitionDecorator.js
@@ -1,4 +1,5 @@
 var utils = require('./Utils');
+var _ = require ('underscore');
 
 function RequisitionDecorator(){
 
@@ -10,91 +11,56 @@ module.exports = gReqDec;
 
 RequisitionDecorator.prototype.addUISchemaToCustomProps = function(requisition, customPropsUISchema){
 	this.customPropsUISchema = customPropsUISchema;
-	var cps = [];
-	if(requisition.customProps){
-		for(var key in requisition.customProps){
-			var cp = this.customPropsUISchema[key];
-			cp.key = key;
-			cp.val = requisition.customProps[key];
-			cps.push(cp);
-		}
-		requisition.customProps = cps;
-	}
+	this.decorate(requisition, this.convertCustomPropsObjectToArray.bind(this));	
+}
+
+RequisitionDecorator.prototype.removeUISchemaFromCutomProps = function(requisition){
+	this.decorate(requisition, this.convertCustomPropsArrayToObject.bind(this));
+}
+
+RequisitionDecorator.prototype.decorate = function(requisition, decorator){
+	decorator(requisition);	
+	if(!utils.hasArray(requisition.Items)) return;
+
 	requisition.Items.forEach((function(item){
-		this.addUISchemaToItemDetailCustomProps(item, 'shipping');
-		this.addUISchemaToItemDetailCustomProps(item, 'others');
-		this.addUISchemaToItemDetailCustomPropsAccounting(item);
+		decorator(item.shipping);
+		decorator(item.others);
+		if(!utils.hasArray(item.accounting)) return;
+			
+		item.accounting.forEach((function(acc){
+	  		decorator(acc);
+	  	}).bind(this));
 	}).bind(this));
 }
 
-RequisitionDecorator.prototype.addUISchemaToItemDetailCustomProps = function(item, itemDetailKey){
-	var cProps = item[itemDetailKey].customProps;
+RequisitionDecorator.prototype.convertCustomPropsObjectToArray = function(parentItem){
+	if(!parentItem) return;
+	var cProps = parentItem.customProps;
 	if(!cProps) return;
 
 	var cps = [];
 	for(var key in cProps){
-		var cp = this.customPropsUISchema[key];
-		cp.key = key;
-		cp.val = item[itemDetailKey].customProps[key];
-		cps.push(cp);
+		cps.push(this.getUICustomPropFromKeyVal(key, parentItem.customProps[key]));
 	}
-	item[itemDetailKey].customProps = cps;
+	parentItem.customProps = cps;
 }
 
-RequisitionDecorator.prototype.addUISchemaToItemDetailCustomPropsAccounting = function(item){
-	if(!item.accounting) return;
-  	if(!Array.isArray(item.accounting)) return;
-  	item.accounting.forEach((function(acc){
-  		var obj = {
-  			acc : {
-  				customProps : acc.customProps
-  			}
-  		};
-  		this.addUISchemaToItemDetailCustomProps(obj, 'acc');
-  		acc.customProps = obj.acc.customProps;
-  	}).bind(this));
-}
-
-RequisitionDecorator.prototype.removeUISchemaFromCutomProps = function(requisition){
-	var self = this;	
-	if(utils.hasArray(requisition.customProps)){
-		var cps = {};
-		requisition.customProps.forEach(function(cp){
-			cps[cp.key] = cp.val;
-		});
-		requisition.customProps = cps;
-	}
-
-	if(utils.hasArray(requisition.Items)){
-		requisition.Items.forEach(function(item){
-			self.removeUISchemaFromItemDetailCustomProps(item, 'shipping');
-			self.removeUISchemaFromItemDetailCustomProps(item, 'others');
-			self.removeUISchemaFromItemDetailCustomPropsAccounting(item);
-		});
-	}
-}
-
-RequisitionDecorator.prototype.removeUISchemaFromItemDetailCustomProps = function(item, itemDetailKey){
-	var cProps = item[itemDetailKey].customProps;
+RequisitionDecorator.prototype.convertCustomPropsArrayToObject = function(parentItem){
+	if(!parentItem) return;
+	var cProps = parentItem.customProps;
 	if(!utils.hasArray(cProps)) return;
 
 	var cps = {};
 	cProps.forEach(function(cp){
 		cps[cp.key] = cp.val;
 	});
-	item[itemDetailKey].customProps = cps;
+	parentItem.customProps = cps;
 }
 
-RequisitionDecorator.prototype.removeUISchemaFromItemDetailCustomPropsAccounting = function(item){
-	if(!utils.hasArray(item.accounting)) return;
-
-	item.accounting.forEach(function(acc){
-		if(utils.hasArray(acc.customProps)){
-			var cps = {};
-			acc.customProps.forEach(function(cp){
-				cps[cp.key] = cp.val;	
-			});
-			acc.customProps = cps;
-		}
-	});
+RequisitionDecorator.prototype.getUICustomPropFromKeyVal = function(key, val){
+	var cp = {key : key};
+	cp = _.extend(cp, this.customPropsUISchema[key]);
+	cp.val = val;
+	return cp;
 }
+
